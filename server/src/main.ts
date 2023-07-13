1d45180caee827b0c14b14993e6316f4c70ee514
Amal21 <amaladded2@gmail.com>
2023-07-14 01:19:19 +0200

    add CORS origin for city endpoints

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,9 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const corsOptions = {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  };
+
+  app.use(cors(corsOptions));
 
   const config = new DocumentBuilder()
     .setTitle('City API')
